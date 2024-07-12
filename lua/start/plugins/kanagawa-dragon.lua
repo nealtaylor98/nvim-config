0ef271ebe926d9a40868547ca4c535590d6c8f66
Taylor Neal <nealtaylor98@gmail.com>
2024-07-12 18:03:12 +1000

    add config

---
+++ lua/start/plugins/kanagawa-dragon.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      vim.cmd.colorscheme 'kanagawa-dragon'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+}
