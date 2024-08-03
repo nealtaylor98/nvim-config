bc5c26f5de76b4a58e202e56b3fbba2acaedfd7e
nealtaylor98 <nealtaylor98@gmail.com>
2024-08-03 21:36:07 +1000

    added html suport

--- lua/start/plugins/kanagawa-dragon.lua
+++ lua/start/plugins/kanagawa-dragon.lua
@@ -3,8 +3,8 @@ return {
     'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.cmd.colorscheme 'kanagawa-dragon'
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'kanagawa-wave'
+      vim.cmd.hi 'Comment gui=italic'
     end,
   },
 }
